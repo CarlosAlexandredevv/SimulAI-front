d292580cc8f99d33a4dec351a21fcb61545c7b52
Carlos Alexandre <alexfilho062005@gmail.com>
2025-10-14 14:35:50 -0300

    :sparkles: get me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+'use client';
 import { ButtonGoogle } from '@/components/ui/button-google';
+import { api } from '@/lib/axios';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    async function getSession() {
+      const response = await api.get('users/me');
+      console.log(response.data);
+    }
+    getSession();
+  }, []);
+
   return (
     <div>
       <ButtonGoogle />
